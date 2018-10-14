10b276cc621e53f622e1fbb6122f753a7bb8b076
Davide Scassola <projanko@gmail.com>
2018-10-14 11:38:20 +0200

    clonata la fork

---
+++ advanced_programming-2018-19/exercises/c++/01_intro/getters.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+
+int get_int()
+{
+   int i;
+   while(!(std::cin >> i))
+   {
+       std::cin.clear();
+       std::cin.ignore();
+   };
+   return i;
+}
+
+double get_double()
+{
+   double d;
+   while(!(std::cin >> d))
+   {
+       std::cin.clear();
+       std::cin.ignore();
+   };
+   return d;
+}
+
+
+int main()
+{
+   double in = get_double();
+   std::cout << in;
+}
